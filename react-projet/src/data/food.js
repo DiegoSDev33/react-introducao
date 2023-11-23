bde0b11730aec4ffdcb7178eada841caa4e8b8b1
Diego Santos <diegodev.silva@gmail.com>
2023-11-22 21:58:17 -0300

    formatacao

--- react-projet/src/data/food.js
+++ react-projet/src/data/food.js
@@ -33,5 +33,49 @@ export const food = [
       "desc": "Fresh & sweet",
       "price": 5,
       "image": "https://images.pexels.com/photos/907142/pexels-photo-907142.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+   },
+   {
+      "id": 4,
+      "title": "Salad",
+      "desc": "Fresh & sweet",
+      "price": 5,
+      "image": "https://images.pexels.com/photos/792028/pexels-photo-792028.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+   },
+   {
+      "id": 5,
+      "title": "Salad",
+      "desc": "Fresh & sweet",
+      "price": 5,
+      "image": "https://images.pexels.com/photos/907142/pexels-photo-907142.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+   }
+   ,
+   {
+      "id": 4,
+      "title": "Salad",
+      "desc": "Fresh & sweet",
+      "price": 5,
+      "image": "https://images.pexels.com/photos/792028/pexels-photo-792028.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+   },
+   {
+      "id": 5,
+      "title": "Salad",
+      "desc": "Fresh & sweet",
+      "price": 5,
+      "image": "https://images.pexels.com/photos/907142/pexels-photo-907142.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+   }
+   ,
+   {
+      "id": 4,
+      "title": "Salad",
+      "desc": "Fresh & sweet",
+      "price": 5,
+      "image": "https://images.pexels.com/photos/792028/pexels-photo-792028.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+   },
+   {
+      "id": 5,
+      "title": "Salad",
+      "desc": "Fresh & sweet",
+      "price": 5,
+      "image": "https://images.pexels.com/photos/907142/pexels-photo-907142.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
    }
 ]
